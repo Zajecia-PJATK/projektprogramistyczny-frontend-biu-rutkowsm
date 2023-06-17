39f4965d543b8251eecf631e304f3174c8ef8151
marcin <s12497@pjwstk.edu.pl>
2023-06-17 15:09:01 +0200

    Register new user + DB insert

--- house-marketplace/src/pages/SignUp.jsx
+++ house-marketplace/src/pages/SignUp.jsx
@@ -26,6 +26,31 @@ function SignUp() {
     }))
   }
 
+  const onSubmit = async (e) => {
+    e.preventDefault()
+
+    try {
+      const auth = getAuth()
+      
+      const userCredential = await createUserWithEmailAndPassword
+      (
+        auth, 
+        email, 
+        password
+      )
+
+      const user =  userCredential.user
+
+      updateProfile(auth.currentUser, {
+        displayName: name
+      })
+
+      navigate('/')
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <>
       <div className="pageContainer">
@@ -36,7 +61,7 @@ function SignUp() {
         </header>
 
         <main>
-          <form>
+          <form onSubmit={onSubmit}> 
             <input 
               type="text" 
               className="nameInput" 
