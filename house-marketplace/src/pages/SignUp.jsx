c74f1ec1ba1ba52f2ea277e13545000f6322f4dc
marcin <s12497@pjwstk.edu.pl>
2023-06-17 14:57:02 +0200

    Firebase.config dir fix

--- house-marketplace/src/pages/SignUp.jsx
+++ house-marketplace/src/pages/SignUp.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import {getAuth, createUserWithEmailAndPassword, updateProfile} from 'firebase/auth'
+import { db } from '../firebase.config'
 import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg' 
 
