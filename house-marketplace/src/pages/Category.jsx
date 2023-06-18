a1adf410290de9f48651e693ca1caa9effb7a0ca
marcin <s12497@pjwstk.edu.pl>
2023-06-19 00:14:58 +0200

    fetch more listings

--- house-marketplace/src/pages/Category.jsx
+++ house-marketplace/src/pages/Category.jsx
@@ -10,6 +10,7 @@ import ListingItem from '../components/ListingItem'
 function Category() {
     const [listings, setListings] = useState(null)
     const [loading, setLoading] = useState(true)
+    const [lastFetchedListing, setLastFetchedListing] = useState(null)
 
     const params = useParams()
 
@@ -21,14 +22,17 @@ function Category() {
 
                 //Build a query
                 const q = query(
-                    listingsRef, 
-                    where('type', '==', params.categoryName), 
+                    listingsRef,
+                    where('type', '==', params.categoryName),
                     orderBy('timestamp', 'desc'),
                     limit(10)
                 )
 
                 //Execute the query
                 const querySnap = await getDocs(q)
+
+                const lastVisible = querySnap.docs[querySnap.docs.length - 1]
+                setLastFetchedListing(lastVisible)
 
                 const listings = []
 
@@ -48,36 +52,82 @@ function Category() {
         fetchListings()
     }, [params.categoryName])
 
-    return (
-        <div className='category'> 
-            <header>
-                <p className="pageHeader">
-                    {params.categoryName === 'rent' 
-                    ? 'Places for rent' 
-                    : 'Places for sale' }
-                </p>
-            </header>
-            {loading ? (
-                <Spinner />
-            ) : listings && listings.length > 0 ? (
-                <>
-                    <main>
-                        <ul className='categoryListings'>
-                            {listings.map((listing) => (
-                                <ListingItem
-                                    listing={listing.data}
-                                    id={listing.id}
-                                    key={listing.id}
-                                />
-                            ))}
-                        </ul>
-                    </main>
-                </>
-            ) : (
-                <p>No listings for {params.categoryName}</p>
-            )}
-        </div>
-    )
-}
+    // Pagination / Load More
+    const onFetchMoreListings = async () => {
+        try {
+            // Get reference
+            const listingsRef = collection(db, 'listings')
 
-export default Category
+            // Create a query
+            const q = query(
+                listingsRef,
+                where('type', '==', params.categoryName),
+                orderBy('timestamp', 'desc'),
+                startAfter(lastFetchedListing),
+                limit(10)
+            )
+
+            // Execute query
+            const querySnap = await getDocs(q)
+
+            const lastVisible = querySnap.docs[querySnap.docs.length - 1]
+            setLastFetchedListing(lastVisible)
+
+            const listings = []
+
+            querySnap.forEach((doc) => {
+                return listings.push({
+                    id: doc.id,
+                    data: doc.data(),
+                })
+            })
+
+            setListings((prevState) => [...prevState, ...listings])
+            setLoading(false)
+        } catch (error) {
+            toast.error('Could not fetch listings')
+        }
+    }
+
+    return (
+        <div className='category'>
+          <header>
+            <p className='pageHeader'>
+              {params.categoryName === 'rent'
+                ? 'Places for rent'
+                : 'Places for sale'}
+            </p>
+          </header>
+    
+          {loading ? (
+            <Spinner />
+          ) : listings && listings.length > 0 ? (
+            <>
+              <main>
+                <ul className='categoryListings'>
+                  {listings.map((listing) => (
+                    <ListingItem
+                      listing={listing.data}
+                      id={listing.id}
+                      key={listing.id}
+                    />
+                  ))}
+                </ul>
+              </main>
+    
+              <br />
+              <br />
+              {lastFetchedListing && (
+                <p className='loadMore' onClick={onFetchMoreListings}>
+                  Load More
+                </p>
+              )}
+            </>
+          ) : (
+            <p>No listings for {params.categoryName}</p>
+          )}
+        </div>
+      )
+    }
+    
+    export default Category
